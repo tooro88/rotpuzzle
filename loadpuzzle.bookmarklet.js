4c78ac2f837c2054057ae48c9499133b3e139a70
tooro88 <tooro88@outlook.jp>
2026-09-17 23:02:33 +0900

    add loadpuzzle

---
+++ loadpuzzle.bookmarklet.js
@@ -0,0 +1,6 @@
+javascript:(()=>{ 
+  const now = Date.now();
+  const s = document.createElement('script');
+  s.src = `https://tooro88.github.io/rotpuzzle/rotpuzzle.bookmarklet.js?${now}`;
+  document.body.appendChild(s);
+})();
